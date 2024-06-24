e0ebe7d0ccc39441e53c77de4b0b22645d4bdd47
Andres Uris <andres.urismontilla@gmail.com>
2024-06-24 13:43:29 -0300

    add get restaurants endpoint

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,15 +1,50 @@
 const fs = require("fs");
+const Mustache = require("mustache");
+const http = require("axios");
 
-const html = fs.readFileSync("static/index.html", "utf-8");
+const restaurantsApiRoot = process.env.restaurants_api;
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+let html;
+
+function loadHtml() {
+    if (!html) {
+        console.log("loading index.html...");
+        html = fs.readFileSync("static/index.html", "utf-8");
+        console.log("loaded");
+    }
+
+    return html;
+}
+
+const getRestaurants = async () => {
+    const resp = await http.get(restaurantsApiRoot);
+
+    return resp.data;
+};
 
 module.exports.handler = async (event, context) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "text/html; charset=UTF-8",
-    },
-    body: html,
-  };
+    const template = loadHtml();
+    const restaurants = await getRestaurants();
+    const dayOfWeek = days[new Date().getDay()];
+    const html = Mustache.render(template, { dayOfWeek, restaurants });
 
-  return response;
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "text/html; charset=UTF-8",
+        },
+        body: html,
+    };
+
+    return response;
 };
